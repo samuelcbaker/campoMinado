fe115f2e9f9ea92c3d61217054051c26094135e7
Samuel <samuelcastro.baker@gmail.com>
2019-11-06 01:25:41 -0300

    Início da lógica

---
+++ app/src/main/java/com/example/campominado/model/Field.kt
@@ -0,0 +1,9 @@
+package com.example.campominado.model
+
+import android.widget.LinearLayout
+import com.example.campominado.utils.Constants
+
+class Field( screenSize: Int, density: Float, columns: Int ){
+    init {
+    }
+}
---
+++ app/src/main/java/com/example/campominado/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.campominado.utils
+
+class Constants{
+    companion object {
+        const val MARGIN_GRID = 40
+        const val MARGIN_ITEM = 10
+    }
+}
